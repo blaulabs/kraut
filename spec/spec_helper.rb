ac7bbeaae9004da1b4e6e0b0cbc016ad8ab32e0f
Thomas Jachmann <self@thomasjachmann.com>
2010-10-22 17:28:20 +0200

    not needed since bundler requires this when using gemspec in Gemfile

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,14 +1,13 @@
-require "rspec"
-require "mocha"
-require "webmock"
+require 'rubygems'
+require 'bundler'
+Bundler.require(:default, :development)
 
-require "savon"
 Savon.log = false
 
 # Requires supporting files.
 Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each { |file| require file }
 
-Rspec.configure do |config|
+RSpec.configure do |config|
   config.mock_with :mocha
   config.include SavonHelper
 end
