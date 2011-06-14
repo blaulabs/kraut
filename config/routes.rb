2e23db6e722addeaa2e8ab88cb09c5e8d94a88b4
Thomas Jachmann <self@thomasjachmann.com>
2011-06-14 14:48:23 +0200

    removes routing deprecation warning

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
-Rails.application.routes.draw do |map|
+Rails.application.routes.draw do
 
   resource :sessions, :controller => "kraut/sessions", :only => [:new, :create, :destroy], :as => "kraut_sessions"
 
